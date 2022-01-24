ebc2f483209bd5b9992f6c4b5a1d437ad5c051f4
WildernessBear <WildernessBear>
2022-01-24 17:52:26 -0500

    parsed out all uppercase words from script

--- findNames.py
+++ findNames.py
@@ -1,15 +1,26 @@
+# findNames.py
+#
+# This file reads the script and finds all the speaking
+# character roles by finding all words that are in all
+# uppercases. The names are put into a list to be used
+# in the parser.
+############################################
+
 import re
 
-print('\n. . . parsing\n')
-
-with open('LOTRs.txt', 'r') as f:
+# with open('LOTRs.txt', 'r') as f:
+with open('LOTRsFellowshipOfTheRing.txt', 'r') as f:
     lines = f.readlines()
 
 
 for line in lines:
-    matches = re.findall(
-        r"(\b(?:[A-Z]+[a-z]?[A-Z]*|[A-Z]*[a-z]?[A-Z]+)\b(?:\s+(?:[A-Z]+[a-z]?[A-Z]*|[A-Z]*[a-z]?[A-Z]+)\b)*)", line)
-    print(matches)
+    # matches = re.findall(
+    #     r"(\b(?:[A-Z]+[a-z]?[A-Z]*|[A-Z]*[a-z]?[A-Z]+)\b(?:\s+(?:[A-Z]+[a-z]?[A-Z]*|[A-Z]*[a-z]?[A-Z]+)\b)*)", line)
+    #     r"(\b[A-Z][A-Z]+\b)", line)
+    #     r"(\b[A-Z]+(?:\s+[A-Z]+)*\b)", line)
 
-    # x = re.search(r"\bS\w+", line)
-    # print(x.group())
+    matches = re.findall(
+        r"(\b[A-Z][A-Z]+(?:\s+[A-Z]+)*\b)", line)
+
+    if len(matches) > 0:
+        print(matches)
