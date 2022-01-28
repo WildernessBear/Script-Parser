85b4d0720a801dd13be13f364ab3b21f215ca4cb
WildernessBear <55259033+WildernessBear@users.noreply.github.com>
2022-01-27 23:38:59 -0500

    finished logic to find if character name is a description or a line block

--- parser.py
+++ parser.py
@@ -6,6 +6,7 @@
 # a nested empty list. The nested list will hold the character lines in
 # the order they appear from the script in *file*.txt
 ##########################################################################
+from lib2to3.pgen2.token import NEWLINE
 import re
 
 # replace with files to be read
@@ -19,11 +20,10 @@ characterList = []
 characterDict = {}
 for name in names:
 
-    # remove newline chars from names in nameFile
+    # remove newline chars from names in nameFile then
+    # save characters in list to check while iterating
+    # through script and also as an empty nested ditionary
     name = name.rstrip()
-
-    # save characters in list to check while iterating through
-    # script and also as an empty nested ditionary
     characterList.append(name)
     characterDict[name] = []
 
@@ -32,48 +32,61 @@ with open(file, 'r') as f:
 
 quote = []
 uppercase = []
-appendLine = False
+startOfLineBlock = False
+keepAppending = False
 i = 0
 
 for line in lines:
 
-    # character name was found
-    if appendLine:
+    # character name was found, add line to quote text
+    if startOfLineBlock:
         quote.append(line)
+
+        # MOVE
+        # quote block is over, append quote to character
+        # list inside character dictionary
         characterDict[uppercase[i]].append(quote)
-        # print(quote)
-        appendLine = False
         quote = []
+        startOfLineBlock = False
+        keepAppending = False
 
     # find uppercase words in script
     uppercase = re.findall(r"(\b[A-Z][A-Z]+(?:\s+[A-Z]+)*\b)", line)
 
-    # see if word is a main character
+    # see if uppercase word is a main character
     i = 0
     index = len(uppercase)
     if index > 0:
         while i < index:
             if uppercase[i] in characterList:
-                appendLine = True
+                foundCharacterName = True
                 break
             i = i+1
 
+        if foundCharacterName == True:
 
-for x, y in characterDict.items():
-    print(x, y)
+            # check that this starts a speaking
+            # section and is not a scene description
+            if re.search("   " + str(uppercase[i]) + "\n", line):
+                block = True
+            else:
+                block = False
+
+            # if it is a voice over (V.O.) then
+            # character name will not be followed by newline
+            voiceOver = re.findall(r"\B\(V\.O\.\)", line)
+
+            # if it is continuation (CONT'D) then
+            # character name will not be followed by newline
+            continuation = re.findall(r"\(CONT\'D\)\B", line)
+
+            # if it is a line off screen (O.S) then
+            # character name will not be followed by newline
+            offScreen = re.findall(r"\(O\.S\.\)\B", line)
+
+            if block or voiceOver or continuation or offScreen:
+                startOfLineBlock == True
 
 
-# if it is a voice over (V.O.) then
-# character name will not be followed by newline
-#voiceOver = re.findall(r"\B\(V\.O\.\)", line)
-
-# if it is continuation (CONT'D) then
-# character name will not be followed by newline
-#continuation = re.findall(r"\(CONT\'D\)\B", line)
-
-# if it is a line off screen (O.S) then
-# character name will not be followed by newline
-#offScreen = re.findall(r"\(O\.S\.\)\B", line)
-
-# if voiceOver or continuation or offScreen:
-#     print(line)
+# for x, y in characterDict.items():
+#     print(x, y)
