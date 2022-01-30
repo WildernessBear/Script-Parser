40dd525fdf2dad5b72a5b4f3ecfc8b6e68fb96ce
WildernessBear <55259033+WildernessBear@users.noreply.github.com>
2022-01-30 15:30:33 -0500

    cleaning up code by making functions

--- parser.py
+++ parser.py
@@ -8,74 +8,92 @@
 ##########################################################################
 import re
 
-# replace with files to be read
-file = 'LOTRsFellowshipOfTheRing.txt'
-nameFile = 'names_LOTRsFellowshipOfTheRing.txt'
+# def readFile(file):
+# read in files
 
-with open(nameFile, 'r') as f:
-    names = f.readlines()
 
-characterList = []
-characterDict = {}
-for name in names:
+def readFile(file):
+    with open(file, 'r') as f:
+        text = f.readlines()
+    return text
 
-    # remove newline chars from names in nameFile then save characters
-    # in list to check while iterating through script and also as an
-    # empty nested ditionary
-    name = name.rstrip()
-    characterList.append(name)
-    characterDict[name] = []
 
-with open(file, 'r') as f:
-    lines = f.readlines()
+# def populateCharacterNames(names):
+# remove newline chars from names in names_*file*.txt then save
+# characters in list to check while iterating through script and
+# also as an empty nested ditionary for saving all dialouge
 
-currName = ''
-quote = ''
-uppercase = []
-inDialogueBlock = False
-endOfDialogueBlock = False
 
-for line in lines:
+def populateCharacterNames(names):
+    characterList = []
+    characterDict = {}
+    for name in names:
+        name = name.rstrip()
+        characterList.append(name)
+        characterDict[name] = []
+    return characterList, characterDict
 
-    # character name was found. Strip additional whitespace from
-    # following line and append to quote text. If \n or empty list
-    # then dialogue is over
-    if inDialogueBlock:
 
-        if line.isspace():
-            inDialogueBlock = False
-            endOfDialogueBlock = True
-        else:
-            line = line.strip()
-            quote = quote + " " + line
+def printPretty(characterDict):
+    for key, value in characterDict.items():
+        e = 1
+        print(key)
+        for a in value:
+            print(str(e) + " " + a)
+            e = e + 1
+    return
 
-    # charcter dialouge ended. Check there is text inside quote
-    # and then append it to the corresponding character in the
-    # dictionary at next index in list
-    if endOfDialogueBlock:
 
-        if len(quote) != 0:
-            characterDict[currName].append(quote)
-        quote = ''
-        inDialogueBlock = False
-        endOfDialogueBlock = False
-        currName = ''
+def main():
 
-    # search for main characters in script
-    for searchName in characterList:
-        if searchName in line:
+    # replace with files to be read
+    lines = readFile('LOTRsFellowshipOfTheRing.txt')
+    names = readFile('names_LOTRsFellowshipOfTheRing.txt')
 
-            # check that this starts a dialogue and is not part of a scene
-            # description, line will only be uppercase letters
-            if line.isupper():
-                inDialogueBlock = True
-                currName = searchName
-            else:
+    characterList, characterDict = populateCharacterNames(names)
+
+    currName = ''
+    quote = ''
+    inDialogueBlock = False
+    endOfDialogueBlock = False
+
+    for line in lines:
+
+        # character name was found. Strip additional whitespace from
+        # following line and append to quote text. If \n or empty list
+        # then dialogue is over
+        if inDialogueBlock:
+
+            if line.isspace():
                 inDialogueBlock = False
+                endOfDialogueBlock = True
+            else:
+                line = line.strip()
+                quote = quote + " " + line
 
-for key, value in characterDict.items():
-    e = 1
-    print(key)
-    for a in value:
-        print(str(e) + " " + a)
-        e = e + 1
+        # charcter dialouge ended. Check there is text inside quote
+        # and then append it to the corresponding character in the
+        # dictionary at next index in list
+        if endOfDialogueBlock:
+
+            if len(quote) != 0:
+                characterDict[currName].append(quote)
+            quote = ''
+            inDialogueBlock = False
+            endOfDialogueBlock = False
+            currName = ''
+
+        # search for main characters in script
+        for searchName in characterList:
+            if searchName in line:
+
+                if line.isupper():
+                    inDialogueBlock = True
+                    currName = searchName
+                else:
+                    inDialogueBlock = False
+
+    printPretty(characterDict)
+
+
+main()
